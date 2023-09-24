e87277b399a152e34ca7b49235a1dbd118b9fcc7
Bassaamm <bassamalqrni@gmail.com>
2023-09-25 00:23:12 +0300

    classes

---
+++ src/Conrtact.java
@@ -0,0 +1,9 @@
+public class Conrtact {
+    public String name;
+    public int phoneNumber;
+    public String emailAddress;
+    public String address;
+    public String birthday;
+    public String notes;
+
+}
