4b757942d04c3824f5cce027fffab331a9d5f8cb
Cryptic <azoozimu@gmail.com>
2023-09-25 02:50:41 +0300

    case 1 updated

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         System.out.println("Welcome to the linked Tree Phonebook!");
-        int option = 0;
+        int option;
         do{
             System.out.println("Please choose an option:");
             System.out.println("1.Add a contact");
@@ -19,13 +19,34 @@ public class Main {
             option = s1.nextInt();
 
             switch (option){
-                case 1->System.out.println("Case 1");
-                case 2->System.out.println("Case 2");
-                case 3->System.out.println("Case 3");
-                case 4->System.out.println("Case 4");
-                case 5->System.out.println("Case 5");
-                case 6->System.out.println("Case 6");
-                case 7->System.out.println("Case 7");
+                case 1 -> {
+                    System.out.print("Enter the contact's name:");
+                    String name = s1.next();
+
+                    System.out.print("Enter the contact's phone number:");
+                    int phoneNumber = s1.nextInt();
+
+                    System.out.print("Enter the contact's email address:");
+                    String email = s1.next();
+
+                    System.out.print("Enter the contact's address:");
+                    String address = s1.next();
+
+                    System.out.print("Enter the contact's birthday:");
+                    String birthday = s1.next();
+
+                    System.out.print("Enter any notes contact: ");
+                    String note = s1.next();
+
+                    Contact newContact = new Contact(name, phoneNumber, email, address, birthday, note);
+
+                }
+                case 2 -> System.out.println("Case 2");
+                case 3 -> System.out.println("Case 3");
+                case 4 -> System.out.println("Case 4");
+                case 5 -> System.out.println("Case 5");
+                case 6 -> System.out.println("Case 6");
+                case 7 -> System.out.println("Case 7");
             }
         }while(option != 8);
         System.out.println();
