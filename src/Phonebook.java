94523a0d347295416fc1b5007e13103256d342d3
Cryptic <azoozimu@gmail.com>
2023-10-01 22:51:33 +0300

    test commit

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,2 +1,23 @@
-public class Phonebook {
+public class Phonebook<T>{
+    LinkedList<Contact> contact;
+    public Phonebook() {
+        LinkedList<Contact> contact = new LinkedList<Contact>();
+    }
+    public boolean add(Contact e) {
+        contact.insert(e);
+        return true;
+    }
+    public boolean search(T check) {
+        Contact head = contact.retrieve();
+
+
+
+        return false;
+    }
+    public boolean remove(Contact e){
+
+
+        return false;
+    }
+
 }
