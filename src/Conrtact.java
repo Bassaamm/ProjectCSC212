5045a8c475cb1454856e1da2da929e7ac8bf9ba6
Cryptic <azoozimu@gmail.com>
2023-10-01 23:48:57 +0300

    Contact reconstruction

--- src/Conrtact.java
+++ src/Conrtact.java
@@ -1,9 +1,65 @@
 public class Conrtact {
-    public String name;
-    public int phoneNumber;
-    public String emailAddress;
-    public String address;
-    public String birthday;
-    public String notes;
+    private String name;
+    private int phoneNumber;
+    private String emailAddress;
+    private String address;
+    private String birthday;
+    private String notes;
 
+    public Conrtact(String name, int phoneNumber, String emailAddress, String address, String birthday, String notes) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+        this.birthday = birthday;
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
