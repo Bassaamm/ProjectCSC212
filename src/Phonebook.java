49f537dcd0dab7ca55c6e0eed30cec7e66c70bc3
Bassaamm <bassamalqrni@gmail.com>
2023-10-04 01:17:24 +0300

    search method

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -15,19 +15,62 @@ public class Phonebook<T>{
         contact.insert(e);
 
     }
-    public boolean search(T check) {
+    public boolean search(int num,T value) {
+    if (contact.empty())
+        return false;
+        contact.findFirst();
+        int length = contact.getLength();
+    Contact head = contact.retrieve();
+        for(int i = 0; i < length ; i++) {
+            if(num == 1)
+            if (head.getName().equalsIgnoreCase((String) value)){
+                System.out.println(value);
+                    printCurrentContact(head);
+                    return true;
+                }
+            else {
+                contact.findNext();
+            };
+            if(num == 2)
+                if ((Integer)head.getPhoneNumber() == value ){
+                    printCurrentContact(head);
+                    return true;
+                } else {
+                    contact.findNext();
+                };
 
+        }
+    return false;
+    }
+    public boolean searchMany(int num,T value) {
+        if (contact.empty())
+            return false;
+        contact.findFirst();
         Contact head = contact.retrieve();
-
-
-
+        for (int i = 0; i < contact.getLength(); i++) {
+            if (num == 3)
+                if (head.getEmailAddress().equalsIgnoreCase((String) value)) {
+                    printCurrentContact(head);
+                    return true;
+                }
+            if (num == 4)
+                if (head.getAddress().equalsIgnoreCase((String) value)) {
+                    printCurrentContact(head);
+                    return true;
+                }
+            if (num == 5)
+                if (head.getBirthday().equalsIgnoreCase((String) value)) {
+                    printCurrentContact(head);
+                    return true;
+                }
+        }
         return false;
     }
     public boolean remove(Contact e){
 
-
         return false;
     }
+
     public void printContacts(){
         if(contact.empty())
             return;
@@ -47,6 +90,14 @@ public class Phonebook<T>{
         System.out.println("Address: " + contact.retrieve().getAddress());
         System.out.println("Birthday: " + contact.retrieve().getBirthday());
         System.out.println("Notes: " + contact.retrieve().getNotes());
+    }
+    private void printCurrentContact(Contact current){
+        System.out.println("Name: " + current.getName());
+        System.out.println("Phone Number: " + current.getPhoneNumber());
+        System.out.println("Email Address: " + current.getEmailAddress());
+        System.out.println("Address: " + current.getAddress());
+        System.out.println("Birthday: " + current.getBirthday());
+        System.out.println("Notes: " + current.getNotes());
     }
 
 }
