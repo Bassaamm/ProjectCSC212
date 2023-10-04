a09d35a579bf1310af0c39b647e4c15b33865bb5
RashedAsh <134154071+X20sM@users.noreply.github.com>
2023-10-04 19:15:44 +0300

    Update on contact
    add event object

---
+++ src/Contact.java
@@ -0,0 +1,76 @@
+public class Contact implements Comparable<Contact>{
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+    private String birthday;
+    private String notes;
+    private Event event[];
+    private int nEvent;
+
+    public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday, String notes,int size) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+        this.birthday = birthday;
+        this.notes = notes;
+        event=new Event[size];
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+
+    public int compareTo(Contact contact) {
+        //x.compareto(y), = 0 (equal), <0 (x before), >0 (y before)
+        return this.name.compareTo(contact.name);
+    }
+
+}
